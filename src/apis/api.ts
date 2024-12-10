2cfdd9b83b21d30bf4171655d91e4c00d8ca183c
yoonjae han <yoonjae.han@obigo.com>
2024-12-10 09:29:24 +0900

    api 호출 헤더 설정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -2,8 +2,15 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // headers: {},
-  baseURL:
-    'http://ec2-54-180-9-141.ap-northeast-2.compute.amazonaws.com:8080/api/v1/',
+  headers: {
+    'Content-Type': 'application/json', // 커스텀 헤더 사용
+    Authorization: 'Bearer token',
+    post: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  },
+  baseURL: 'https://was.woowang.store/api/v1/',
 });
 
 axiosInstance.interceptors.request.use(
